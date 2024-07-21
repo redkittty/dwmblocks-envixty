309f140d11b738c9ade551853db35de407206ec5
redkittty <114357508+redkittty@users.noreply.github.com>
2024-07-21 19:11:16 -0400

    Made memory icon use Font Awesome
    [ICONS]
    - Replaced Nerd Font memory icon with Font Awesome memory icon
    
    [DEPENDENCIES]
    - Added Font Awesome Dependency

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {" 󰮯 PKG: ", "$HOME/.config/suckless/dwmblocks/scripts/pacupdate",     360,  9},
+  {"󰮯 PKG: ", "pacupdate",     360,  9},
 
-	{" RAM:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" RAM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{" TIME: ", "date '+%b %d (%a) %I:%M%p '",					5,		0},
+	{"  TIME: ", "date '+%a, %b %d - %I:%M%p'",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
